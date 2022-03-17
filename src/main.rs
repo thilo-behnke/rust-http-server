727eb99d057b3758bf5882af7979265a7b941339
Thilo Behnke <thilo.behnke@gmx.net>
2022-03-17 22:41:19 +0100

    working get to html

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
-use std::io::{Read};
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-const MESSAGE_SIZE: usize = 5;
+const MESSAGE_SIZE: usize = 1024;
 
 fn main() -> std::io::Result<()> {
     println!("Starting tcp bind to 8080.");
@@ -37,10 +37,12 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
                 }
                 received.extend_from_slice(&buf[..bytes_read]);
                 message = std::str::from_utf8(&received).expect("invalid ut8");
-                let terminated = message.ends_with("\n\n");
+                println!("Read message: {}", message);
+                let terminated = message.ends_with("\r\n\r\n");
                 if terminated {
                     println!("Received terminated message: {}", message);
-                    try_parse_http_request(message)
+                    process_http_request(message, &stream);
+                    received = vec![];
                 }
             }
             Err(e) => {
@@ -52,32 +54,61 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     Ok(())
 }
 
-// fn try_parse_http_request(request: String) -> std::io::Result<HttpRequest>  {
-fn try_parse_http_request(request: &str) {
-    let request_lines: Vec<&str> = request.split("\\n").collect();
-    let method_line = request_lines.first();
-    let header_lines = request_lines[1..];
-    match method_line {
-        Some(&val) => match val.split(" ").collect::<Vec<&str>>().first() {
-            Some(&"GET") => {
-                println!("Found GET request")
+fn process_http_request(message: &str, mut out_stream: &TcpStream) {
+    let request = try_parse_http_request(message);
+    match request {
+        Ok(req) => match (req.method, req.path.as_str()) {
+            (HttpMethod::GET, "/") => {
+                println!("Sending response to client");
+                let content = "<div>test<div>";
+                let res = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", content.len(), content);
+                out_stream.write(res.as_bytes()).unwrap();
+                out_stream.flush().unwrap();
             },
-            _ => println!("Failed to parse method of http request: {}", val)
+            _ => println!("noop")
         },
-        None => println!("Unable to split request into lines")
+        Err(e) => println!("noop")
     }
+}
+
+fn try_parse_http_request(request: &str) -> Result<HttpRequest, &str> {
+    let request_lines: Vec<Vec<&str>> = request.split("\r\n").map(|line| line.split(' ').collect()).collect();
+    let request = match request_lines.as_slice() {
+        [] => Err("Empty request"),
+        // [m] => Some(HttpRequest {method: m[0], path: m[1], headers: HashMap::new()}),
+        [m, rest @ ..] => Ok(HttpRequest {method: http_method_from_string(m[0]).expect("Invalid http method"), path: String::from(m[1]), headers: HashMap::new()})
+    };
+    return match request {
+        Ok(req) => {
+            println!("HttpRequest [method={:?}, path={:?}]", req.method, req.path);
+            Ok(req)
+        },
+        Err(e) => {
+            println!("{}", e);
+            Err(e)
+        }
+    };
 }
 
 fn try_parse_get_request(request_lines: Vec<&str>) {
 
 }
 
+fn http_method_from_string(method: &str) -> Result<HttpMethod, &str> {
+    return match method {
+        "GET" => Ok(HttpMethod::GET),
+        "POST" => Ok(HttpMethod::POST),
+        _ => Err("Invalid http method")
+    }
+}
+
 struct HttpRequest {
     method: HttpMethod,
     path: String,
-    headers: HashMap<str, str>
+    headers: HashMap<String, String>
 }
 
+#[derive(Debug)]
 enum HttpMethod {
     GET, POST
 }
